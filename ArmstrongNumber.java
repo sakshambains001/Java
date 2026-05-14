f7fdd7802aa200febdd09408ece559b6dda3e896
sakshambains001 <sakshambains80@gmail.com>
2026-05-14 16:24:11 +0530

    Add files via upload

---
+++ ArmstrongNumber.java
@@ -0,0 +1,19 @@
+// 1. Armstrong Number Checker
+public class ArmstrongNumber {
+    public static void main(String[] args) {
+        int number = 153;
+        int originalNumber = number;
+        int sum = 0;
+        int digits = String.valueOf(number).length();
+        while (number != 0) {
+            int digit = number % 10;
+            sum += Math.pow(digit, digits);
+            number /= 10;
+        }
+        if (sum == originalNumber) {
+            System.out.println(originalNumber + " is an Armstrong number.");
+        } else {
+            System.out.println(originalNumber + " is not an Armstrong number.");
+        }
+    }
+}
